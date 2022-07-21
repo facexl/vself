e7811cccb79351f5ef1dedd60f74c439ecff9321
xiaolang <facelanglang@gmail.com>
2022-07-21 18:07:42 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+        imports: ['vue','vue-router'],
+    })
+ ],
+ resolve:{
+    alias:{
+        '@': path.resolve(__dirname,'src'),
+    },
+    extensions:['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'elementlib',
+      fileName: (format) => `elementlib.${format}.js`
+    },
+    sourcemap: true,
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: [
+        'vue'
+     ],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
+
