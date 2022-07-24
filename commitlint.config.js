9e51728d90a3615b2e8cc7f5a874416334ac1a44
xiaolang <facelanglang@gmail.com>
2022-07-24 14:43:29 +0800

    chore: config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,1 @@
-// commitlint uses `ts-node` to load typescript config, it's too slow. So we replace it with `esbuild`.
-require('@esbuild-kit/cjs-loader')
-module.exports = require('./commitlint.config.ts').default
+module.exports = { extends: ['@commitlint/config-conventional'] };
